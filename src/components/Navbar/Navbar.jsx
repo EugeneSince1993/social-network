c3b38f9570c90cbbdead677e94d7f5b1ebbeb30e
EugeneSince1993 <jk235@mail.ru>
2021-11-22 21:07:04 +0300

    lesson 20 passed

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,23 +1,38 @@
 import React from 'react';
 import s from './Navbar.module.css';
+import { NavLink } from "react-router-dom";
+
+const isNavLinkActive = ({ isActive }) => {
+    return { color: isActive ? "gold" : "" } ;
+};
 
 const Navbar = () => {
   return (
     <nav className={s.nav}>
       <div className={s.item}>
-        <a href="/profile">Profile</a>
-      </div>
-      <div className={`${s.item} ${s.active}`}>
-        <a href="/dialogs">Messages</a>
-      </div>
-      <div className={s.item}>
-        <a href="/news">News</a>
+        <NavLink to="/profile" style={isNavLinkActive}>
+            Profile
+        </NavLink>
       </div>
       <div className={s.item}>
-        <a href="/music">Music</a>
+        <NavLink to="/dialogs" style={isNavLinkActive}>
+            Messages
+        </NavLink>
       </div>
       <div className={s.item}>
-        <a href="/settings">Settings</a>
+        <NavLink to="/news" style={isNavLinkActive}>
+            News
+        </NavLink>
+      </div>
+      <div className={s.item}>
+        <NavLink to="/music" style={isNavLinkActive}>
+            Music
+        </NavLink>
+      </div>
+      <div className={s.item}>
+        <NavLink to="/settings" style={isNavLinkActive}>
+            Settings
+        </NavLink>
       </div>
     </nav>
   );
