9c63cebebe736c1d48206def22ee202e1d882a97
EugeneSince1993 <jk235@mail.ru>
2021-11-30 01:09:24 +0300

    lesson 26 - task done

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Music from "./components/Music/Music";
 import Settings from "./components/Settings/Settings";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-const App = () => {
+const App = (props) => {
   return (
       <BrowserRouter>
           <div className='app-wrapper'>
@@ -17,8 +17,16 @@ const App = () => {
               <Navbar />
               <div className='app-wrapper-content'>
                   <Routes>
-                      <Route path='/dialogs' element={<Dialogs />} />
-                      <Route path='/profile' element={<Profile />} />
+                      <Route
+                          path='/dialogs'
+                          element={
+                              <Dialogs
+                                dialogsArray={props.dialogsArray}
+                                messagesArray={props.messagesArray}
+                              />
+                          }
+                      />
+                      <Route path='/profile' element={<Profile postsArray={props.postsArray} />} />
                       <Route path='/news' element={<News />} />
                       <Route path='/music' element={<Music />} />
                       <Route path='/settings' element={<Settings />} />
--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -5,26 +5,9 @@ import Message from "./Message/Message";
 
 const Dialogs = (props) => {
 
-    let dialogs = [
-        { id: 1, name: 'Dimych' },
-        { id: 2, name: 'Andrey' },
-        { id: 3, name: 'Sveta' },
-        { id: 4, name: 'Sasha' },
-        { id: 5, name: 'Viktor' },
-        { id: 6, name: 'Valera' }
-    ];
+    let dialogsElements = props.dialogsArray.map( d => <DialogItem name={d.name} id={d.id} /> );
 
-    let messages = [
-        { id: 1, message: 'Hi' },
-        { id: 2, message: 'How is your it-kamasutra?' },
-        { id: 3, message: 'Yo' },
-        { id: 4, message: 'Yo' },
-        { id: 5, message: 'Yo' }
-    ];
-
-    let dialogsElements = dialogs.map( d => <DialogItem name={d.name} id={d.id} /> );
-
-    let messagesElements = messages.map( m => <Message message={m.message} id={m.id} /> );
+    let messagesElements = props.messagesArray.map( m => <Message message={m.message} id={m.id} /> );
 
     return (
         <div className={s.dialogs}>
