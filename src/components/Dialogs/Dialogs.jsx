5f11b476e0115af1f887b148b1fc04a3b2f00f1a
EugeneSince1993 <jk235@mail.ru>
2021-12-02 18:25:06 +0300

    lesson 31 passed and task done

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -11,6 +11,13 @@ const Dialogs = (props) => {
 
   let messagesElements = props.state.messages.map(m => <Message message={m.message} id={m.id}/>);
 
+  let newPostElement = React.createRef();
+
+  let addPost = () => {
+    let text = newPostElement.current.value;
+    alert(text);
+  };
+
   return (
     <div className={s.dialogs}>
       <div className={s.dialogsItems}>
@@ -18,6 +25,12 @@ const Dialogs = (props) => {
       </div>
       <div className={s.messages}>
         {messagesElements}
+        <div>
+          <textarea ref={newPostElement}></textarea>
+        </div>
+        <div>
+          <button onClick={addPost}>Add post</button>
+        </div>
       </div>
     </div>
   );
