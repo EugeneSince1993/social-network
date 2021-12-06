01129d5f15097b174526a03f916a48289e45ee5b
EugeneSince1993 <jk235@mail.ru>
2021-12-07 00:11:30 +0300

    lesson 34 - stopped at 30-40

--- src/redux/state.js
+++ src/redux/state.js
@@ -32,10 +32,12 @@ let state = {
   }
 };
 
-export let addPost = (postMessage) => {
+window.state = state;
+
+export let addPost = () => {
   let newPost = {
     id: 5,
-    message: postMessage,
+    message: state.profilePage.newPostText,
     likesCount: 0
   };
   state.profilePage.posts.push(newPost);
