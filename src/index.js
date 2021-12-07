140f0eab8d0af939c307dcd0108c05edf48cabe9
EugeneSince1993 <jk235@mail.ru>
2021-12-08 02:22:43 +0300

    lesson 35 - stopped at 5-50

--- src/index.js
+++ src/index.js
@@ -1,6 +1,26 @@
 import * as serviceWorker from './serviceWorker';
 import state from './redux/state';
-import {rerenderEntireTree} from "./render";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import {addMessage, addPost, updateNewMessageText, updateNewPostText} from './redux/state';
+import {BrowserRouter} from "react-router-dom";
+
+let rerenderEntireTree = (state) => {
+  ReactDOM.render(
+    <BrowserRouter>
+      <App
+        state={state}
+        addPost={addPost}
+        updateNewPostText={updateNewPostText}
+        addMessage={addMessage}
+        updateNewMessageText={updateNewMessageText}
+      />
+    </BrowserRouter>,
+    document.getElementById('root')
+  );
+};
 
 rerenderEntireTree(state);
 
