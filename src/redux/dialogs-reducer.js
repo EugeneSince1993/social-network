c4be862db21eeb0dc7f6639b78aae4d42a7edc82
EugeneSince1993 <jk235@mail.ru>
2022-01-20 11:38:43 +0300

    lesson 42 passed

--- src/redux/dialogs-reducer.js
+++ src/redux/dialogs-reducer.js
@@ -1,7 +1,26 @@
 const SEND_MESSAGE = 'SEND-MESSAGE';
 const UPDATE_NEW_MESSAGE_BODY = 'UPDATE-NEW-MESSAGE-BODY';
 
-const dialogsReducer = (state, action) => {
+let initialState = {
+  dialogs: [
+    {id: 1, name: 'Dimych', avatarName: '2.jpg'},
+    {id: 2, name: 'Andrey', avatarName: '4.jpg'},
+    {id: 3, name: 'Sveta', avatarName: '1.jpg'},
+    {id: 4, name: 'Sasha', avatarName: '6.jpg'},
+    {id: 5, name: 'Viktor', avatarName: '9.jpg'},
+    {id: 6, name: 'Valera', avatarName: '8.jpg'}
+  ],
+  messages: [
+    {id: 1, message: 'Hi'},
+    {id: 2, message: 'How is your it-kamasutra?'},
+    {id: 3, message: 'Yo'},
+    {id: 4, message: 'Yo'},
+    {id: 5, message: 'Yo'}
+  ],
+  newMessageBody: ""
+};
+
+const dialogsReducer = (state = initialState, action) => {
   switch (action.type) {
     case UPDATE_NEW_MESSAGE_BODY:
       state.newMessageBody = action.body;
