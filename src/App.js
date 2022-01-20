304a4ef38e961b9a62c5e1b961bcbcabdecc6a2f
EugeneSince1993 <jk235@mail.ru>
2022-01-20 14:22:33 +0300

    lesson 43 passed

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import './App.css';
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
-import Dialogs from "./components/Dialogs/Dialogs";
 import News from "./components/News/News";
 import Music from "./components/Music/Music";
 import Settings from "./components/Settings/Settings";
 import {Routes, Route} from "react-router-dom";
+import DialogsContainer from "./components/Dialogs/DialogsContainer";
 
 const App = (props) => {
   return (
@@ -18,20 +18,11 @@ const App = (props) => {
         <Routes>
           <Route
             path='/dialogs'
-            element={
-              <Dialogs
-                store={props.store}
-              />
-            }
+            element={<DialogsContainer store={props.store} />}
           />
           <Route
             path='/profile'
-            element={
-              <Profile
-                profilePage={props.state.profilePage}
-                dispatch={props.dispatch}
-              />
-            }
+            element={<Profile store={props.store} />}
           />
           <Route path='/news' element={<News/>}/>
           <Route path='/music' element={<Music/>}/>
