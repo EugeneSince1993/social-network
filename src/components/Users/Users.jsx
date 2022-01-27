fd7f50ac9f45e779e78c25dd55b3107004ee1d2c
EugeneSince1993 <jk235@mail.ru>
2022-01-27 16:20:51 +0300

    lesson 49 paused at 49-00

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,9 +1,33 @@
 import React from 'react';
+import styles from './users.module.css';
 
 const Users = (props) => {
   return (
     <div>
-      USERS WILL BE HERE
+      {
+        props.users.map(u => <div key={u.id}>
+            <span>
+              <div>
+                <img src={u.photoUrl} className={styles.userPhoto} />
+              {/* 49.00 */}
+              </div>
+              <div>
+                <button>Follow</button>
+              </div>
+            </span>
+            <span>
+              <span>
+                <div>{u.fullName}</div>
+                <div>{u.status}</div>
+              </span>
+              <span>
+                <div>{u.location.country}</div>
+                <div>{u.location.city}</div>
+              </span>
+            </span>
+          </div>
+        )
+      }
     </div>
   );
 };
--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Users from "./Users";
 import {connect} from "react-redux";
-import {followAC, unfollowAC} from "../../redux/users-reducer";
+import {followAC, setUsersAC, unfollowAC} from "../../redux/users-reducer";
 
 let mapStateToProps = (state) => {
   return {
@@ -16,8 +16,10 @@ let mapDispatchToProps = (dispatch) => {
     },
     unfollow: (userId) => {
       dispatch(unfollowAC(userId));
+    },
+    setUsers: (users) => {
+      dispatch(setUsersAC(users));
     }
-    // 40-00 setUsers
   };
 };
 
