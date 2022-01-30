787e50027746295e93f6861e81162d3bc8c48aca
EugeneSince1993 <jk235@mail.ru>
2022-01-30 14:17:48 +0300

    lesson 60 paused at 15-55

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -3,10 +3,32 @@ import Profile from "./Profile";
 import * as axios from "axios";
 import {connect} from "react-redux";
 import {setUserProfile} from "../../redux/profile-reducer";
+import {
+  useLocation,
+  useNavigate,
+  useParams
+} from "react-router-dom";
+
+function withRouter(Component) {
+  function ComponentWithRouterProp(props) {
+    let location = useLocation();
+    let navigate = useNavigate();
+    let params = useParams();
+    return (
+      <Component
+        {...props}
+        router={{ location, navigate, params }}
+      />
+    );
+  }
+
+  return ComponentWithRouterProp;
+}
 
 class ProfileContainer extends React.Component {
 
   componentDidMount() {
+
     axios.get(`https://social-network.samuraijs.com/api/1.0/profile/2`)
       .then(response => {
         this.props.setUserProfile(response.data);
@@ -24,4 +46,7 @@ let mapStateToProps = (state) => ({
   profile: state.profilePage.profile
 });
 
-export default connect(mapStateToProps, { setUserProfile } )(ProfileContainer);
+let WithUrlDataContainerComponent = withRouter(ProfileContainer);
+
+export default connect(mapStateToProps, { setUserProfile } )(WithUrlDataContainerComponent);
+// 15.55
