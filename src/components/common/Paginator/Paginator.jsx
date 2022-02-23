f22bf97fdeef7fe064e2ed1e7ca725da45977c49
EugeneSince1993 <jk235@mail.ru>
2022-02-23 23:11:03 +0300

    lesson 93 paused at 14-50

--- src/components/common/Paginator/Paginator.jsx
+++ src/components/common/Paginator/Paginator.jsx
@@ -1,8 +1,8 @@
-import React from "react";
+import React, {useState} from "react";
 import styles from "./Paginator.module.css";
 
-const Paginator = ({totalUsersCount, pageSize, currentPage, onPageChanged}) => {
-  let pagesCount = Math.ceil(totalUsersCount / pageSize);
+const Paginator = ({totalItemsCount, pageSize, currentPage, onPageChanged, portionSize = 10}) => {
+  let pagesCount = Math.ceil(totalItemsCount / pageSize);
 
   let pages = [];
 
@@ -10,16 +10,27 @@ const Paginator = ({totalUsersCount, pageSize, currentPage, onPageChanged}) => {
     pages.push(i);
   }
 
+  let portionCount = Math.ceil(pagesCount / portionSize);
+  // 14.50
+  let [portionNumber, setPortionNumber] = useState(1);
+  let leftPortionPageNumber = (portionNumber - 1) * portionSize + 1;
+  let rightPortionPageNumber = portionNumber * portionSize;
+
   return (
     <div>
-      { pages.map( p => {
-        return (
-          <span
-            className={ currentPage === p && styles.selectedPage }
-            onClick={ (e) => { onPageChanged(p); } }>
-              {p}
-          </span>);
-      } ) }
+
+      {
+        pages
+          .filter(p => p >= leftPortionPageNumber && p <= rightPortionPageNumber)
+          .map( p => {
+          return (
+            <span
+              className={ currentPage === p && styles.selectedPage }
+              onClick={ (e) => { onPageChanged(p); } }>
+                {p}
+            </span>);
+          } )
+      }
     </div>
   );
 };
